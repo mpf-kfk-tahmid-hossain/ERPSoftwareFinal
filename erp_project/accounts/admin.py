f6bf686f265faf554279ced822684a086620f445
Tahmid881 <syedmdtahmidhossain@gmail.com>
2025-06-19 22:15:20 +0400

    Added Permission and Role Permission to the Admin Console

--- erp_project/accounts/admin.py
+++ erp_project/accounts/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from .models import Company, Role, User, UserRole
+from .models import Company, Role, User, UserRole, RolePermission, Permission
 
 admin.site.register(Company)
 admin.site.register(Role)
 admin.site.register(User, DjangoUserAdmin)
 admin.site.register(UserRole)
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('codename', 'description')
+    search_fields = ('codename', 'description')
+
+@admin.register(RolePermission)
+class RolePermissionAdmin(admin.ModelAdmin):
+    list_display = ('role', 'permission')
+    list_filter = ('role', 'permission')
+    search_fields = ('role__name', 'permission__codename')
