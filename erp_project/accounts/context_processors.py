d16e7918de0a19d478bdf478dd79b013dc9972ac
mpf-kfk-tahmid-hossain <syedmdtahmidhossain@gmail.com>
2025-06-20 20:09:13 +0400

    Remove has_permission filter from templates

---
+++ erp_project/accounts/context_processors.py
@@ -0,0 +1,24 @@
+from types import SimpleNamespace
+from .utils import user_has_permission
+
+
+def nav_permissions(request):
+    """Return permission flags for navigation menu items."""
+    if not request.user.is_authenticated or not getattr(request.user, 'company', None):
+        return {}
+    codes = [
+        'view_user',
+        'view_role',
+        'view_warehouse',
+        'view_productcategory',
+        'view_productunit',
+        'view_product',
+        'view_stocklot',
+        'view_stockmovement',
+        'view_inventoryadjustment',
+        'view_stock_on_hand',
+        'view_auditlog',
+    ]
+    perms = {code: user_has_permission(request.user, code) for code in codes}
+    return {'nav_perms': SimpleNamespace(**perms)}
+
